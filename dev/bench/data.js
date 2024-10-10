02c3b2451c8c36aff87370fc71cc2dbe00e32e25
github-action-benchmark <github@users.noreply.github.com>
2024-10-10 17:36:50 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 58038d1ea21c0ffa556d4a6f0eff858b0ab21075

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728581783659,
+  "lastUpdate": 1728581810217,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -40,6 +40,52 @@ window.BENCHMARK_DATA = {
             "value": 26392.148345947266,
             "unit": "ns",
             "range": "± 184.94365324743055"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "58038d1ea21c0ffa556d4a6f0eff858b0ab21075",
+          "message": "Moved the permissions to full workflow",
+          "timestamp": "2024-10-10T17:32:02Z",
+          "url": "https://github.com/microsoft/garnet/commit/58038d1ea21c0ffa556d4a6f0eff858b0ab21075"
+        },
+        "date": 1728581807524,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.Get",
+            "value": 46656.871484375,
+            "unit": "ns",
+            "range": "± 333.7071372577903"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.Set",
+            "value": 47641.68767496745,
+            "unit": "ns",
+            "range": "± 300.1799642202174"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.MGet",
+            "value": 42175.07598673503,
+            "unit": "ns",
+            "range": "± 227.34783744095733"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.MSet",
+            "value": 40254.09579702524,
+            "unit": "ns",
+            "range": "± 231.5822070551467"
           }
         ]
       }
