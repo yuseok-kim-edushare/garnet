08a800a8198ef9d0c83ef1d617be9a263cc36ed8
github-action-benchmark <github@users.noreply.github.com>
2024-10-14 23:46:15 +0000

    add BDNBenchmark - Cluster.RespClusterBench (os ubuntu-latest, framework net8.0, configuration Release) (benchmarkdotnet) benchmark result for f8ddb6cefd5c0c6ccc561469c8f6a16bd2c9c055

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728949574229,
+  "lastUpdate": 1728949575840,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "BDNBenchmark - Resp.RespTsavoriteStress (os ubuntu-latest, framework net8.0, configuration Release)": [
@@ -354,6 +354,52 @@ window.BENCHMARK_DATA = {
             "value": 18793.25958486704,
             "unit": "ns",
             "range": "± 28.85585852473372"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "f8ddb6cefd5c0c6ccc561469c8f6a16bd2c9c055",
+          "message": "A few fixes",
+          "timestamp": "2024-10-14T23:42:25Z",
+          "url": "https://github.com/microsoft/garnet/commit/f8ddb6cefd5c0c6ccc561469c8f6a16bd2c9c055"
+        },
+        "date": 1728949572066,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterBench.Get",
+            "value": 26757.65296282087,
+            "unit": "ns",
+            "range": "± 82.96631594732229"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterBench.Set",
+            "value": 25581.577154541017,
+            "unit": "ns",
+            "range": "± 88.61692126141025"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterBench.MGet",
+            "value": 20900.254941304524,
+            "unit": "ns",
+            "range": "± 9.03919675056658"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterBench.MSet",
+            "value": 18941.89795328776,
+            "unit": "ns",
+            "range": "± 141.02584737812813"
           }
         ]
       }
