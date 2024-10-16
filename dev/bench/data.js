e600e011c98a42ed52bde92feedf0dff21eb7a71
github-action-benchmark <github@users.noreply.github.com>
2024-10-16 22:38:00 +0000

    add Resp.RespLuaStress (windows-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 23296d620d07bd76f2649bf1b9fc10ebaac777a1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729118277498,
+  "lastUpdate": 1729118279958,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -4738,6 +4738,52 @@ window.BENCHMARK_DATA = {
             "value": 1640.0049754551478,
             "unit": "ns",
             "range": "± 4.640914400765225"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "23296d620d07bd76f2649bf1b9fc10ebaac777a1",
+          "message": "Set threshhold to 50% (150%) as seeing BDN ran twice on same code sometimes is in 40%+ range of difference.",
+          "timestamp": "2024-10-16T22:32:23Z",
+          "url": "https://github.com/microsoft/garnet/commit/23296d620d07bd76f2649bf1b9fc10ebaac777a1"
+        },
+        "date": 1729118276095,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespLuaStress.BasicLuaStress1",
+            "value": 103.16452554294041,
+            "unit": "ns",
+            "range": "± 0.24173866558537513"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespLuaStress.BasicLuaStress2",
+            "value": 174.39732892172677,
+            "unit": "ns",
+            "range": "± 0.4178658511737561"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespLuaStress.BasicLuaStress3",
+            "value": 701.8474170139858,
+            "unit": "ns",
+            "range": "± 1.8593981194891291"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespLuaStress.BasicLuaStress4",
+            "value": 1699.619129725865,
+            "unit": "ns",
+            "range": "± 3.098001579322543"
           }
         ]
       }
