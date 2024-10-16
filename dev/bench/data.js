871a2d8f7be7a8a53567a70bec89961e75a31bac
github-action-benchmark <github@users.noreply.github.com>
2024-10-16 20:19:05 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 400872685cf465e258c8501fdccd937fb805fda5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729109941505,
+  "lastUpdate": 1729109944906,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -40,6 +40,52 @@ window.BENCHMARK_DATA = {
             "value": 26768.03887023926,
             "unit": "ns",
             "range": "± 139.75101476898845"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "400872685cf465e258c8501fdccd937fb805fda5",
+          "message": "Versioning issue with BDN test files, fixed now",
+          "timestamp": "2024-10-16T20:10:45Z",
+          "url": "https://github.com/microsoft/garnet/commit/400872685cf465e258c8501fdccd937fb805fda5"
+        },
+        "date": 1729109942749,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterBench.Get",
+            "value": 26128.334458414713,
+            "unit": "ns",
+            "range": "± 194.56346412433106"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterBench.Set",
+            "value": 25692.575028483072,
+            "unit": "ns",
+            "range": "± 247.80744502248476"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterBench.MGet",
+            "value": 20243.551000976564,
+            "unit": "ns",
+            "range": "± 149.15288432647387"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterBench.MSet",
+            "value": 18979.73750406901,
+            "unit": "ns",
+            "range": "± 227.65848312969786"
           }
         ]
       }
