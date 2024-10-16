ad13e1ce4de43c438e59cbaef773047e3b0f4948
github-action-benchmark <github@users.noreply.github.com>
2024-10-16 22:18:36 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 28ccbb4548fe59778ece8ff7c6b834b992cd5ddd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729116774243,
+  "lastUpdate": 1729117116853,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -1460,6 +1460,46 @@ window.BENCHMARK_DATA = {
             "value": 252640.2622767857,
             "unit": "ns",
             "range": "± 639.3803208406856"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "28ccbb4548fe59778ece8ff7c6b834b992cd5ddd",
+          "message": "Putting it back to both OS for the tests",
+          "timestamp": "2024-10-16T22:15:26Z",
+          "url": "https://github.com/microsoft/garnet/commit/28ccbb4548fe59778ece8ff7c6b834b992cd5ddd"
+        },
+        "date": 1729117114120,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Get",
+            "value": 20169.828043619793,
+            "unit": "ns",
+            "range": "± 25.328474849669828"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Set",
+            "value": 19573.830552164713,
+            "unit": "ns",
+            "range": "± 210.01633885870237"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Incr",
+            "value": 28199.14892272949,
+            "unit": "ns",
+            "range": "± 175.72248386180675"
           }
         ]
       }
