961827d6a8b32cb08fce1846d0488f4122722196
github-action-benchmark <github@users.noreply.github.com>
2024-10-16 20:19:01 +0000

    add Resp.RespTsavoriteStress (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 400872685cf465e258c8501fdccd937fb805fda5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729109938738,
+  "lastUpdate": 1729109941505,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -21,6 +21,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/microsoft/garnet/commit/400872685cf465e258c8501fdccd937fb805fda5"
         },
         "date": 1729109935489,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Get",
+            "value": 20364.048573811848,
+            "unit": "ns",
+            "range": "± 111.31696084092498"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Set",
+            "value": 19571.894126383464,
+            "unit": "ns",
+            "range": "± 200.5534366997758"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Incr",
+            "value": 26768.03887023926,
+            "unit": "ns",
+            "range": "± 139.75101476898845"
+          }
+        ]
+      }
+    ],
+    "Resp.RespTsavoriteStress (ubuntu-latest  net8.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "400872685cf465e258c8501fdccd937fb805fda5",
+          "message": "Versioning issue with BDN test files, fixed now",
+          "timestamp": "2024-10-16T20:10:45Z",
+          "url": "https://github.com/microsoft/garnet/commit/400872685cf465e258c8501fdccd937fb805fda5"
+        },
+        "date": 1729109940554,
         "tool": "benchmarkdotnet",
         "benches": [
           {
