b657d9f3036dc5502c8b451990e2446990659ef8
github-action-benchmark <github@users.noreply.github.com>
2024-10-16 20:57:48 +0000

    add Benchmark (benchmarkdotnet) benchmark result for cdf2d25356fad2ca234a01969809b3e4d3aeb5fb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729112265614,
+  "lastUpdate": 1729112268083,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -184,6 +184,46 @@ window.BENCHMARK_DATA = {
             "value": 2245.1390329996743,
             "unit": "ns",
             "range": "± 6.059954867905853"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "cdf2d25356fad2ca234a01969809b3e4d3aeb5fb",
+          "message": "Bump threshold to 35% (135%) to trigger perf failure",
+          "timestamp": "2024-10-16T20:52:20Z",
+          "url": "https://github.com/microsoft/garnet/commit/cdf2d25356fad2ca234a01969809b3e4d3aeb5fb"
+        },
+        "date": 1729112252742,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Get",
+            "value": 19831.358102651742,
+            "unit": "ns",
+            "range": "± 17.824902860249885"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Set",
+            "value": 18474.219403948104,
+            "unit": "ns",
+            "range": "± 68.43236397147602"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Incr",
+            "value": 26288.77454485212,
+            "unit": "ns",
+            "range": "± 100.62564507580122"
           }
         ]
       }
