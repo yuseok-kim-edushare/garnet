9b7741265a86340a05b88dc60771167e0edb12f6
github-action-benchmark <github@users.noreply.github.com>
2024-10-18 16:50:48 +0000

    add Resp.RespTsavoriteStress (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for df16e9c7648a231044813267042dc711b540ca25

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729270247010,
+  "lastUpdate": 1729270248644,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -21,6 +21,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/microsoft/garnet/commit/df16e9c7648a231044813267042dc711b540ca25"
         },
         "date": 1729270245560,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Get",
+            "value": 21266.587079366047,
+            "unit": "ns",
+            "range": "± 19.08534813790571"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Set",
+            "value": 18954.090964181083,
+            "unit": "ns",
+            "range": "± 86.30786113695392"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Incr",
+            "value": 29014.32657950265,
+            "unit": "ns",
+            "range": "± 165.75302972259325"
+          }
+        ]
+      }
+    ],
+    "Resp.RespTsavoriteStress (ubuntu-latest  net8.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "df16e9c7648a231044813267042dc711b540ca25",
+          "message": "Merge branch 'main' into darrenge/BDNPerf",
+          "timestamp": "2024-10-18T16:46:31Z",
+          "url": "https://github.com/microsoft/garnet/commit/df16e9c7648a231044813267042dc711b540ca25"
+        },
+        "date": 1729270248326,
         "tool": "benchmarkdotnet",
         "benches": [
           {
