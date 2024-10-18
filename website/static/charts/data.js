308ae5d8d4cb6407d9696babc579d8914685166c
github-action-benchmark <github@users.noreply.github.com>
2024-10-18 18:08:02 +0000

    add Benchmark (benchmarkdotnet) benchmark result for d58bf0d17e760cd279f1e5415d24d0aabb830aa9

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729274847223,
+  "lastUpdate": 1729274882382,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -1824,6 +1824,52 @@ window.BENCHMARK_DATA = {
             "value": 28635.067772420247,
             "unit": "ns",
             "range": "± 175.923004989089"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "d58bf0d17e760cd279f1e5415d24d0aabb830aa9",
+          "message": "Bumping threshold to 50% (150%) as mean value in RespParseStress can vary into the 40% from run to run.",
+          "timestamp": "2024-10-18T18:03:04Z",
+          "url": "https://github.com/microsoft/garnet/commit/d58bf0d17e760cd279f1e5415d24d0aabb830aa9"
+        },
+        "date": 1729274881130,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.Get",
+            "value": 46433.06945800781,
+            "unit": "ns",
+            "range": "± 26.161400664233508"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.Set",
+            "value": 49953.909815470375,
+            "unit": "ns",
+            "range": "± 52.341178582521806"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.MGet",
+            "value": 44396.79704402043,
+            "unit": "ns",
+            "range": "± 56.69054589475997"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.MSet",
+            "value": 41764.633697509766,
+            "unit": "ns",
+            "range": "± 42.85679262830047"
           }
         ]
       }
