f402430ab00e79fcf6599dd3888925f2987aa8db
github-action-benchmark <github@users.noreply.github.com>
2024-10-28 21:39:08 +0000

    add Operations.BasicOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 665bda1ae92aaff071c7d8c05bb2edd11b23c0a7

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730151547028,
+  "lastUpdate": 1730151548407,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -21,6 +21,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/microsoft/garnet/commit/665bda1ae92aaff071c7d8c05bb2edd11b23c0a7"
         },
         "date": 1730151545590,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1773.2309645244054,
+            "unit": "ns",
+            "range": "± 12.277929914728604"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1782.6699559529623,
+            "unit": "ns",
+            "range": "± 23.155922794419347"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1611.885228597201,
+            "unit": "ns",
+            "range": "± 5.102283478084043"
+          }
+        ]
+      }
+    ],
+    "Operations.BasicOperations (ubuntu-latest  net8.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "665bda1ae92aaff071c7d8c05bb2edd11b23c0a7",
+          "message": "Fix bug in ObjectOperations",
+          "timestamp": "2024-10-28T21:22:42Z",
+          "url": "https://github.com/microsoft/garnet/commit/665bda1ae92aaff071c7d8c05bb2edd11b23c0a7"
+        },
+        "date": 1730151548142,
         "tool": "benchmarkdotnet",
         "benches": [
           {
