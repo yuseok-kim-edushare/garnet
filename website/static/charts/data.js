f916e15bf7be1c35d5de2b1f2f89a6705a62e5b4
github-action-benchmark <github@users.noreply.github.com>
2024-10-28 21:28:32 +0000

    add Operations.BasicOperations (windows-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 665bda1ae92aaff071c7d8c05bb2edd11b23c0a7

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730150907893,
+  "lastUpdate": 1730150911970,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -5724,6 +5724,46 @@ window.BENCHMARK_DATA = {
             "value": 1769.067808787028,
             "unit": "ns",
             "range": "± 2.818599081271463"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "665bda1ae92aaff071c7d8c05bb2edd11b23c0a7",
+          "message": "Fix bug in ObjectOperations",
+          "timestamp": "2024-10-28T21:22:42Z",
+          "url": "https://github.com/microsoft/garnet/commit/665bda1ae92aaff071c7d8c05bb2edd11b23c0a7"
+        },
+        "date": 1730150910384,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1768.9173562186104,
+            "unit": "ns",
+            "range": "± 2.1706543262262064"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1858.9623377873347,
+            "unit": "ns",
+            "range": "± 1.5570261416621436"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1766.0154887608119,
+            "unit": "ns",
+            "range": "± 3.2147120599691594"
           }
         ]
       }
