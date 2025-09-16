fa5f2e94146ac7cddbb2f9d5de80961ae4a92b7c
github-action-benchmark <github@users.noreply.github.com>
2025-09-16 11:53:04 +0000

    add Operations.BasicOperations (windows-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 294e46b3b353251115bbbc824dda8ef6db878c96

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758023369567,
+  "lastUpdate": 1758023583849,
   "repoUrl": "https://github.com/yuseok-kim-edushare/garnet",
   "entries": {
     "Benchmark": [
@@ -9286,6 +9286,50 @@ window.BENCHMARK_DATA = {
             "value": 371.5423075358073,
             "unit": "ns",
             "range": "± 0.473633544796957"
+          }
+        ]
+      }
+    ],
+    "Operations.BasicOperations (windows-latest  net9.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "email": "22998716+KiKoS0@users.noreply.github.com",
+            "name": "Riadh",
+            "username": "KiKoS0"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "294e46b3b353251115bbbc824dda8ef6db878c96",
+          "message": "Fix ZCOUNT error when min bound is higher than set's max score (#1375)\n\nCo-authored-by: Tal Zaccai <talzacc@microsoft.com>",
+          "timestamp": "2025-09-09T22:02:08-07:00",
+          "tree_id": "acccb0ffddd1731ba00eea768ed530747161b875",
+          "url": "https://github.com/yuseok-kim-edushare/garnet/commit/294e46b3b353251115bbbc824dda8ef6db878c96"
+        },
+        "date": 1758023576078,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1796.7546609731821,
+            "unit": "ns",
+            "range": "± 1.9016591242139047"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1766.8513706752233,
+            "unit": "ns",
+            "range": "± 2.780570306931353"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1797.0984935760498,
+            "unit": "ns",
+            "range": "± 1.2275244283673103"
           }
         ]
       }
