93ec7d0dc3ccfc6a7ea4e363bd73f16463dc1778
github-action-benchmark <github@users.noreply.github.com>
2025-09-16 11:17:25 +0000

    add Cluster.ClusterMigrate (ubuntu-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 294e46b3b353251115bbbc824dda8ef6db878c96

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758021402813,
+  "lastUpdate": 1758021445211,
   "repoUrl": "https://github.com/yuseok-kim-edushare/garnet",
   "entries": {
     "Benchmark": [
@@ -7888,6 +7888,56 @@ window.BENCHMARK_DATA = {
             "value": 17594.26008605957,
             "unit": "ns",
             "range": "± 10.916326708330049"
+          }
+        ]
+      }
+    ],
+    "Cluster.ClusterMigrate (ubuntu-latest  net9.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "email": "22998716+KiKoS0@users.noreply.github.com",
+            "name": "Riadh",
+            "username": "KiKoS0"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "294e46b3b353251115bbbc824dda8ef6db878c96",
+          "message": "Fix ZCOUNT error when min bound is higher than set's max score (#1375)\n\nCo-authored-by: Tal Zaccai <talzacc@microsoft.com>",
+          "timestamp": "2025-09-09T22:02:08-07:00",
+          "tree_id": "acccb0ffddd1731ba00eea768ed530747161b875",
+          "url": "https://github.com/yuseok-kim-edushare/garnet/commit/294e46b3b353251115bbbc824dda8ef6db878c96"
+        },
+        "date": 1758021442971,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Get(Params: None)",
+            "value": 35674.52711704799,
+            "unit": "ns",
+            "range": "± 65.2327401466479"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Set(Params: None)",
+            "value": 36069.29597981771,
+            "unit": "ns",
+            "range": "± 57.70363664168598"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MGet(Params: None)",
+            "value": 31139.115652901786,
+            "unit": "ns",
+            "range": "± 26.060394853888372"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MSet(Params: None)",
+            "value": 29903.53725382487,
+            "unit": "ns",
+            "range": "± 19.31376257740609"
           }
         ]
       }
