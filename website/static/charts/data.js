d647359f81503b9fc749e8bf2d3619ede0e5bc00
github-action-benchmark <github@users.noreply.github.com>
2025-09-16 11:16:42 +0000

    add Operations.PubSubOperations (ubuntu-latest  net9.0 Release) (benchmarkdotnet) benchmark result for 294e46b3b353251115bbbc824dda8ef6db878c96

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758021331900,
+  "lastUpdate": 1758021402813,
   "repoUrl": "https://github.com/yuseok-kim-edushare/garnet",
   "entries": {
     "Benchmark": [
@@ -7844,6 +7844,50 @@ window.BENCHMARK_DATA = {
             "value": 187869.83829752603,
             "unit": "ns",
             "range": "± 1083.027307160235"
+          }
+        ]
+      }
+    ],
+    "Operations.PubSubOperations (ubuntu-latest  net9.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "email": "22998716+KiKoS0@users.noreply.github.com",
+            "name": "Riadh",
+            "username": "KiKoS0"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "294e46b3b353251115bbbc824dda8ef6db878c96",
+          "message": "Fix ZCOUNT error when min bound is higher than set's max score (#1375)\n\nCo-authored-by: Tal Zaccai <talzacc@microsoft.com>",
+          "timestamp": "2025-09-09T22:02:08-07:00",
+          "tree_id": "acccb0ffddd1731ba00eea768ed530747161b875",
+          "url": "https://github.com/yuseok-kim-edushare/garnet/commit/294e46b3b353251115bbbc824dda8ef6db878c96"
+        },
+        "date": 1758021401056,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 17871.087661743164,
+            "unit": "ns",
+            "range": "± 12.427871985841161"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 17740.874442545573,
+            "unit": "ns",
+            "range": "± 19.67318480607245"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 17594.26008605957,
+            "unit": "ns",
+            "range": "± 10.916326708330049"
           }
         ]
       }
